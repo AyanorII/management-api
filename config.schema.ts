f2d4c0898ab0cefd19b3d474f34017dad0669b21
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-23 12:45:58 +0900

    add config service and env schema validation

---
+++ config.schema.ts
@@ -0,0 +1,7 @@
+import * as Joi from '@hapi/joi';
+
+export const configValidationSchema = Joi.object({
+  DB_URL: Joi.string().required(),
+  PORT: Joi.number().default(8000),
+  JWT_SECRET: Joi.string().required(),
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from '../config.schema';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: configValidationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
