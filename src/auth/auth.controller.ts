464a82b16a8a397d630ce6f628f52031bbc551fd
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-23 14:24:01 +0900

    add auth login and signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { SignUpDto } from './dto/signup.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('/signup')
+  async signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../users/users.service';
+import { LoginDto } from './dto/login.dto';
+import { SignUpDto } from './dto/signup.dto';
+import { AccessToken } from './interfaces';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
 
-  async createUser() {
-    
+  async login(loginDto: LoginDto): Promise<AccessToken> {
+    const user = await this.usersService.findOne(loginDto.email);
+
+    const passwordMatches = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (passwordMatches) {
+      const payload = {
+        email: user.email,
+        sub: user.id,
+      };
+
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    }
+
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  async signUp(signUpDto: SignUpDto): Promise<AccessToken> {
+    const user = await this.usersService.create(signUpDto);
+    const payload = {
+      email: user.email,
+      sub: user.id,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
