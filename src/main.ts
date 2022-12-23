2deadef16d455e90ed3170ecfa3e52a757ed5a49
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-23 13:57:15 +0900

    add swagger documentation and global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Config service
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  // Swagger API documentation
+  const config = new DocumentBuilder()
+    .setTitle('Management API')
+    .setVersion('1.0')
+    .addTag('Management')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Pipes
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
 }
