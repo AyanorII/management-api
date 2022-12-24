b4e7413fe8e7bc2bcbe8d5138cc90f13766a2618
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-24 10:28:22 +0900

    add employee business logic

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,1 +1,36 @@
-export class CreateEmployeeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Gender } from '@prisma/client';
+import {
+  IsBoolean,
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+export class CreateEmployeeDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @ApiProperty()
+  @IsString()
+  phone: string;
+
+  @ApiProperty()
+  @IsDateString()
+  startedAt: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  active: boolean;
+
+  @ApiProperty()
+  @IsNumber()
+  salary: number;
+}
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Employee } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Injectable()
 export class EmployeesService {
-  create(createEmployeeDto: CreateEmployeeDto) {
-    return 'This action adds a new employee';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
+    const startedAt = new Date(createEmployeeDto.startedAt);
+
+    const employee = await this.prisma.employee.create({
+      data: {
+        ...createEmployeeDto,
+        startedAt,
+      },
+    });
+
+    return employee;
   }
 
-  findAll() {
-    return `This action returns all employees`;
+  async findAll(): Promise<Employee[]> {
+    const employees = await this.prisma.employee.findMany({
+      orderBy: {
+        active: 'desc',
+      },
+    });
+    return employees;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} employee`;
+  async findOne(id: number): Promise<Employee> {
+    const employee = await this.prisma.employee.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!employee) {
+      throw new NotFoundException(`Employee #${id} not found`);
+    }
+
+    return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
-    return `This action updates a #${id} employee`;
+  async update(
+    id: number,
+    updateEmployeeDto: UpdateEmployeeDto,
+  ): Promise<Employee> {
+    const employee = await this.prisma.employee.update({
+      where: {
+        id,
+      },
+      data: updateEmployeeDto,
+    });
+
+    return employee;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} employee`;
+  async remove(id: number): Promise<void> {
+    await this.prisma.employee.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
