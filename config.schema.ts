09883f50534f15548c79a0af9579671d610a02af
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-30 16:46:41 +0900

    refactor cloudinary service and specify the folder to upload

--- config.schema.ts
+++ config.schema.ts
@@ -1,15 +1,23 @@
 import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
+  // Database
   DB_URL: Joi.string().required(),
+
+  // Server
   PORT: Joi.number().default(8000),
-  JWT_SECRET: Joi.string().required(),
-  JWT_REFRESH_SECRET: Joi.string().required(),
   CORS_ORIGIN: Joi.string().required(),
   ENV: Joi.string().valid('development', 'production').default('development'),
   THROTTLE_TTL: Joi.number().default(60),
   THROTTLE_LIMIT: Joi.number().default(10),
+
+  // JWT
+  JWT_SECRET: Joi.string().required(),
+  JWT_REFRESH_SECRET: Joi.string().required(),
+
+  // Cloudinary
   CLOUDINARY_CLOUD_NAME: Joi.string().required(),
   CLOUDINARY_API_KEY: Joi.string().required(),
   CLOUDINARY_API_SECRET: Joi.string(),
+  CLOUDINARY_FOLDER: Joi.string(),
 });
--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,22 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import * as fs from 'fs';
 
 @Injectable()
 export class CloudinaryService {
+  constructor(private readonly configService: ConfigService) {}
+
   async uploadImage(
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
-    return new Promise((resolve, reject) => {
-      v2.uploader.upload(file.path, {}, (error, result) => {
-        if (error) return reject(error);
-        resolve(result);
-        // Delete the file after it has been uploaded to Cloudinary
-        fs.unlink(file.path, (err) => {
-          console.error(err);
-          if (err) throw err;
-        });
+    try {
+      const result = await v2.uploader.upload(file.path, {
+        folder: this.configService.get('CLOUDINARY_FOLDER'),
       });
-    });
+
+      // Delete local image after upload
+      fs.unlink(file.path, (err) => {
+        console.error(err);
+        if (err) throw err;
+      });
+
+      return result;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  /**
+   * Deletes image from Cloudinary. Throws Bad Request (400) if no image was found.
+   * @param url
+   */
+  async deleteImage(url: string): Promise<void> {
+    const publicId = this.getPublicIdFromUrl(url);
+
+    try {
+      const { result } = await v2.uploader.destroy(publicId);
+
+      if (result === 'not found') {
+        throw new BadRequestException(
+          `Image with public id '${publicId}' not found`,
+        );
+      }
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  /**
+   * Extracts the public ID from the url of the image. The public ID consists of:
+   * 1 - The folder the image is stored
+   * 2 - The image ID
+   * @param url The URL of the image hosted on Cloudinary
+   * @returns The public ID of the image
+   * @example "https://res.cloudinary.com/ayanorii/image/upload/v1672384346/management-app-development/k1stsmmuilpv70rfiwdl.png" -> "management-app-development/k1stsmmuilpv70rfiwdl.png"
+   */
+  getPublicIdFromUrl(url: string): string {
+    return url
+      .split('/')
+      .slice(-2)
+      .join('/')
+      .split(/\.(png)|(jpg)|(jpeg)/)[0];
   }
 }
