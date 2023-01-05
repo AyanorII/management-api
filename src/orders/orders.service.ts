55d5d6aba6bab7d0345dcd0d1a0c328fe53d9d63
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-01-05 14:13:14 +0900

    add orders service create

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Order } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrderItemsService } from './order-items.service';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly orderItemsService: OrderItemsService,
+  ) {}
+
+  async create(createOrderDto: CreateOrderDto): Promise<Order> {
+    const { orderItems, vendorId, ...data } = createOrderDto;
+
+    const items = await this.orderItemsService.buildMany(orderItems);
+    const total = items.reduce((acc, item) => acc + item.subtotal, 0);
+
+    const order = await this.prisma.order.create({
+      data: {
+        ...data,
+        total,
+        orderItems: {
+          createMany: {
+            data: items,
+          },
+        },
+        vendor: {
+          connect: {
+            id: vendorId,
+          },
+        },
+      },
+      include: {
+        vendor: true,
+        orderItems: true,
+      },
+    });
+    return order;
   }
 
   findAll() {
