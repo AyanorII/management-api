78b26a893e1611f9340a504206b8dd372181053c
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-02-27 17:53:05 +0900

    refactor UsersModule and UsersService with MongoDB

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,119 +3,70 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { User } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import { FilterQuery } from 'mongoose';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UserWithoutPassword } from './interfaces';
+import { UserDocument } from './schemas/user.schema';
+import { UsersRepository } from './users.repository';
+{
+}
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly usersRepository: UsersRepository,
+  ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<UserWithoutPassword> {
-    const { email, password, name, companyName } = createUserDto;
-    await this._checkIfUserExists(email);
-
-    const hashedPassword = await this._hashPassword(password);
-
-    const user = await this.prisma.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-        name,
-        companyName,
-      },
-      select: {
-        id: true,
-        email: true,
-        companyName: true,
-        name: true,
-        createdAt: true,
-        updatedAt: true,
-        refreshToken: true,
-      },
-    });
-
-    return user;
+  async create(createUserDto: CreateUserDto): Promise<UserDocument> {
+    try {
+      const user = await this.usersRepository.create(createUserDto);
+      delete user.password;
+      return user;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException('User already exists');
+      }
+    }
   }
 
-  async findOneByEmail(email: string): Promise<User> {
-    const user = await this._findOneBy('email', email);
-    return user;
+  async findAll(): Promise<UserDocument[]> {
+    const users = await this.usersRepository.findAll();
+
+    users.forEach((user) => delete user.password);
+
+    return users;
   }
 
-  async findOneById(id: number): Promise<User> {
-    const user = await this._findOneBy('id', id);
+  async findOne(filterQuery: FilterQuery<UserDocument>): Promise<UserDocument> {
+    const user = await this.usersRepository.findOne(filterQuery);
+
+    if (!user) {
+      throw new NotFoundException(`User not found`);
+    }
+
+    delete user.password;
+
     return user;
   }
 
   async update(
-    id: number,
+    id: string,
     updateUserDto: UpdateUserDto,
-  ): Promise<UserWithoutPassword> {
-    const updatedUser = await this.prisma.user.update({
-      where: {
-        id,
-      },
-      data: updateUserDto,
-    });
-
-    const { password, ...user } = updatedUser;
-
-    return user;
-  }
-
-  async removeRefreshToken(id: number): Promise<void> {
-    // Using updateMany because we need to update only the user that has a refresh token.
-    const user = await this.prisma.user.updateMany({
-      where: {
-        id,
-        refreshToken: {
-          not: null,
-        },
-      },
-      data: {
-        refreshToken: null,
-      },
-    });
-
-    if (user.count === 0) {
-      throw new NotFoundException('User not found');
-    }
-  }
-
-  // Private
-  private async _checkIfUserExists(email: string): Promise<void> {
-    const user = await this.prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (user) {
-      throw new BadRequestException('User already exists');
-    }
-  }
-
-  private async _hashPassword(password: string): Promise<string> {
-    const salt = await bcrypt.genSalt();
-    return bcrypt.hash(password, salt);
-  }
-
-  private async _findOneBy(
-    field: string,
-    value: string | number,
-  ): Promise<User> {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        [field]: value,
-      },
-    });
+  ): Promise<UserDocument> {
+    const user = await this.usersRepository.update(id, updateUserDto);
 
     if (!user) {
-      throw new NotFoundException(`User with ${field} '${value}' not found`);
+      throw new NotFoundException(`User not found`);
     }
 
+    delete user.password;
+
     return user;
+  }
+
+  async removeRefreshToken(id: string): Promise<void> {
+    await this.update(id, { refreshToken: null });
   }
 }
